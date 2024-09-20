12c8df67af2df6e78314854b99e41e4c147b90f9
fou3fou3 <foxedex29@gmail.com>
2024-09-20 12:25:41 +0100

    rocket init

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
+#[macro_use]
+extern crate rocket;
 mod db;
 
 use db::init_db;
 
-// const DB_NAME: &str = "copernicus";
 const CONNECTION_STRING: &str = "mysql://admin:admin@localhost:3306/copernicus";
 
-#[tokio::main]
-async fn main() {
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+// #[get("/api/signin")]
+// fn api_signin() -> &'static str {
+//     "Hello, world!"
+// }
+
+#[launch]
+async fn rocket() -> _ {
     let _ = init_db(CONNECTION_STRING).await.unwrap();
+
+    rocket::build().mount("/", routes![index])
 }
